02e756d51cc5cd0030d255a8ceb2b391645a8988
cytoncode <cytoncode@gmail.com>
2021-05-19 12:45:08 +0300

    Added configuration file
    All variables are stored here, the presence of which is necessary for starting and, if necessary, compiling the program.

---
+++ ant/configuration.py
@@ -0,0 +1,4 @@
+name = "ant"  # the name of the command we type in ANT
+compile = ""  # if empty is not executed
+execute = "python main.py"  # the string that ANT sends in bach to execute
+arguments = []  # arguments that can be passed by default
